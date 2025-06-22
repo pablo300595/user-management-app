5723ec6a17f52006718565adc8f2a642fa6d35a9
Pablo Valenzuela <pabloeng05@gmail.com>
2025-06-21 18:31:37 -0700

    feat: Apply Login Page Styles

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-login',
-  imports: [MatFormFieldModule, FormsModule, MatInputModule, MatSlideToggleModule, MatButtonModule],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private _formBuilder = inject(FormBuilder);
   checked = false;
-  disabled = false;
+
+  loginFormGroup = this._formBuilder.group({
+    username: ['', Validators.requiredTrue],
+    password: ['', Validators.requiredTrue],
+    showPassword: [false, Validators.requiredTrue],
+  });
+
 }
